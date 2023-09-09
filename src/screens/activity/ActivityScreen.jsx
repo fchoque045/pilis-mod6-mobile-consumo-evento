74efb2803968a4fbcc2370ae4152eb2f9680bcee
Fabian Choque <fabianchoque28@gmail.com>
2023-09-08 21:23:09 -0300

    consulta de transacciones por cliente

--- src/screens/activity/ActivityScreen.jsx
+++ src/screens/activity/ActivityScreen.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Text, View, SafeAreaView, ScrollView, RefreshControl, FlatList } from 'react-native'
 import { styles } from './ActivityScreen.styles'
+import { getListTransactions } from '../../api/consumo-evento-api'
+import { UserContext } from '../../contexts/UserContext'
+import { showToastError } from '../../utils/toast'
+import moment from 'moment'
 
 export const ActivityScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Activity Screen</Text>
+
+  const {currentUser} = useContext(UserContext)
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    getListTransactions(currentUser.wallet, currentUser.token).then(data => {
+      if (data.message) {
+        return showToastError('Algo Salió Mal', data.message)
+      }
+      data.map((t) => {
+        t.date = dateFormated(t.date)
+      })
+      setTransactions(data)
+    }).catch(err => {
+      showToastError('Fallo en la Conexion', 'Su Saldo no pudo ser Actualizada')
+      console.warn(err)
+    })
+  }, [])
+
+  const dateFormated = (date_string) => {
+    const date = moment(date_string);
+    const dateFormated = date.format('YYYY-MM-DD');
+
+    return dateFormated
+  }
+  // const fetchWallet = () => {
+  //   getWalletUser(currentUser.user.id, currentUser.token).then(data => {
+  //     if (data.message) {
+  //       return showToastError('Algo Salió Mal', data.message)
+  //     }
+  //     setWallet(data)
+  //     setRefreshing(false)
+  //   }).catch(err => {
+  //     showToastError('Fallo en la Conexion', 'Su Saldo no pudo ser Actualizada')
+  //     setRefreshing(false)
+  //     console.warn(err)
+  //   })
+  // }
+
+  const location = ({ item }) => (
+    <View style={styles.cardView}>
+        <View style={styles.containerTransaction}>
+          <View>
+            <Text style={styles.titleBusiness}>{item.business.name}</Text>
+            <Text style={styles.titleLocation}>local {item.business.location}</Text>
+          </View>
+          <View>
+            <Text style={styles.titleBusiness}>${item.amount}</Text>
+            <Text style={styles.titleLocation}>{item.date}</Text>
+          </View>
+        </View>
     </View>
+  )
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Compras</Text>
+        <FlatList
+          data={transactions}
+          renderItem={location}
+          keyExtractor={item => item.id}
+          style={styles.itemList}
+        />
+         
+    </SafeAreaView>
   )
 }
--- src/screens/activity/ActivityScreen.styles.js
+++ src/screens/activity/ActivityScreen.styles.js
@@ -10,8 +10,38 @@ export const styles = StyleSheet.create({
   title: {
     fontSize: FONT_SIZE.lg,
     fontWeight: 'bold',
+    padding: 10,
     color: COLORS.text,
     width: '100%',
     marginLeft: 25
+  },
+  cardView: {
+    margin: 10,
+    backgroundColor: '#EDE0D2',
+    borderRadius: 10,
+    padding: 20,
+    overflow: 'hidden',
+    alignItems: 'flex-start',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  containerTransaction: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  titleBusiness: {
+    fontSize: FONT_SIZE.md,
+    fontWeight: 'bold',
+    color: COLORS.text,
+    width: '100%',
+  },
+  titleLocation: {
+    fontSize: FONT_SIZE.md,
+    color: COLORS.text,
+    width: '100%',
   }
 })
